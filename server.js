bc2d46dd1600d41c42d6e85d0e83ab344596e459
DhanushPenugonda <dhanush.penugonda@gmail.com>
2023-05-01 11:25:16 +0530

    login half done

--- server.js
+++ server.js
@@ -8,10 +8,41 @@ app.use(express.static(path.resolve('public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 3000;
 
+var user="";
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
 
-app.get('/', (req, res) => {
-    res.render("home");
+app.get('/student', (req, res) => {
+    res.render("student/home",{user: user});
+});
+app.get('/faculty', (req, res) => {
+    res.render("faculty/home",{user: user});
+});
+app.get('/parent', (req, res) => {
+    res.render("parent/home",{user: user});
+});
+app.get('/admin', (req, res) => {
+    res.render("admin/home",{user: user});
+});
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+app.post('/login', (req, res) => {
+    user = req.body['user-type'];
+    if(user == "student"){
+        res.redirect("/student");
+    }
+    else if(user == "faculty"){
+        res.redirect("/faculty");
+    }
+    else if(user == "parent"){
+        res.redirect("/parent");
+    }
+    else{
+        res.redirect("/admin");
+    }
+
 });
